1894b9e85ab5124c43d8b153ddb7cfdb459cfb92
lechithien191919-jpg <lechithien191919@gmail.com>
2026-09-25 21:38:58 +0700

    Update world.js

--- js/world.js
+++ js/world.js
@@ -12,9 +12,9 @@ export class World {
     }
 
     generateFlatWorld() {
-        // Tạo sàn phẳng 11x11 an toàn
-        for (let x = -5; x <= 5; x++) {
-            for (let z = -5; z <= 5; z++) {
+        // Tạo mặt sàn phẳng gọn gàng (Grass ở trên, Dirt và Stone ở dưới)
+        for (let x = -4; x <= 4; x++) {
+            for (let z = -4; z <= 4; z++) {
                 this.addBlock(x, 0, z, BLOCK_TYPES.GRASS);
                 this.addBlock(x, -1, z, BLOCK_TYPES.DIRT);
                 this.addBlock(x, -2, z, BLOCK_TYPES.STONE);
@@ -35,23 +35,5 @@ export class World {
         
         this.scene.add(mesh);
         this.blocks.set(key, mesh);
-    }
-
-    removeBlock(mesh) {
-        if (!mesh || !mesh.parent) return;
-        
-        const x = mesh.position.x;
-        const y = mesh.position.y;
-        const z = mesh.position.z;
-        const key = this.getKey(x, y, z);
-
-        this.scene.remove(mesh);
-        mesh.geometry.dispose();
-        this.blocks.delete(key);
-    }
-
-    getBlockAt(x, y, z) {
-        const key = this.getKey(x, y, z);
-        return this.blocks.get(key) || null;
     }
 }
