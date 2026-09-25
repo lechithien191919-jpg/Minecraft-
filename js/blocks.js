92a4db2486700c9056419f4cc61f4d82583b98b8
lechithien191919-jpg <lechithien191919@gmail.com>
2026-09-25 21:43:59 +0700

    Update blocks.js

--- js/blocks.js
+++ js/blocks.js
@@ -4,12 +4,20 @@ export const BLOCK_TYPES = {
     STONE: 'stone'
 };
 
+// Hàm tạo vật liệu 3D cho từng loại block
 export function getBlockMaterial(type) {
     let color = 0x888888;
     switch (type) {
-        case BLOCK_TYPES.GRASS: color = 0x559933; break; // Xanh lá cỏ Minecraft
-        case BLOCK_TYPES.DIRT:  color = 0x8b5a2b; break; // Nâu đất
-        case BLOCK_TYPES.STONE: color = 0x7f7f7f; break; // Xám đá
+        case BLOCK_TYPES.GRASS: 
+            color = 0x559933; // Xanh lá đặc trưng của cỏ
+            break; 
+        case BLOCK_TYPES.DIRT:  
+            color = 0x8b5a2b; // Nâu đất
+            break; 
+        case BLOCK_TYPES.STONE: 
+            color = 0x7f7f7f; // Xám đá
+            break;
     }
+    // Dùng MeshLambertMaterial để bắt sáng 3D chân thực
     return new THREE.MeshLambertMaterial({ color: color });
 }
